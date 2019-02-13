b8dc4136cc974bdf36b718b9419574e8e806ac14
Chitose Ruri <1034414291@qq.com>
2019-02-13 15:20:58 +0800

    Hello Lua

--- HelloLua/HelloLua/main.cpp
+++ HelloLua/HelloLua/main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+	lua_State * l = luaL_newstate();
+	luaL_openlibs(l);
+	luaL_dofile(l, "main.lua");
+	lua_close(l);
+	system("pause");
 	return 0;
 }
--- HelloLua/HelloLua/main.h
+++ HelloLua/HelloLua/main.h
@@ -1,9 +1,10 @@
 #pragma once
 //connect to lua
+#include <iostream>
+#include <lua.hpp>
+#include <lua.h>
 #if defined(DEBUG) | defined(_DEBUG)
 #pragma comment(lib,"../../lua-5.3.5/lib/lua_5.3.5_D.lib")
 #else
 #pragma comment(lib,"../../lua-5.3.5/lib/lua_5.3.5_R.lib")
 #endif
-
-#include "lua.h"
