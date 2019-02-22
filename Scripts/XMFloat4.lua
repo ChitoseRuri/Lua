747347ca2f5fd98625ce430a5c6f913d19176557
ChitoseRuri <1034414291@qq.com>
2019-02-22 11:42:46 +0800

    Update XMFloat4.lua

--- Scripts/XMFloat4.lua
+++ Scripts/XMFloat4.lua
@@ -1,5 +1,5 @@
 XMFLoat4 = {x,y,z,w}; -- members
-XMFLoat4.mt = {}; -- metafunction
+XMFLoat4.mt = {}; -- metamethods
 function  XMFLoat4.new( initList ) -- construct
 	local outList = {};
 	setmetatable(outList,XMFLoat4.mt);
@@ -36,17 +36,27 @@ function XMFLoat4.div( ls, rs )
 	return nil
 end
 
-function XMFLoat4.unm( ... )
-	-- body
+function XMFLoat4.unm(rs)
+	local outList = {};
+	outList.x = !rs.x;
+	outList.y = !rs.y;
+	outList.z = !rs.z;
+	outList.w = !rs.w;
+	return outList;
+end
+
+function XMFLoat4.tostring( rs )
+	return "{"..(rs.x or 0).." ,"..(rs.y or 0).." ,"..(rs.z or 0).." ,"..(rs.w or 0).."}";
 end
 
 XMFLoat4.mt.__add = XMFLoat4.add;
 XMFLoat4.mt.__sub = XMFLoat4.sub;
---[[	 test
+XMFLoat4.mt.__mul = XMFLoat4.mul;
+XMFLoat4.mt.__div = XMFLoat4.div;
+XMFLoat4.mt.__tostring = XMFLoat4.tostring;
 a = XMFLoat4.new({1,2,3,4});
 b = XMFLoat4.new({5,6,7,8});
 c = a + b;
 d = a - b;
-print(c.w);
-print(d.w);
---]]
+print(c);
+print(d);
